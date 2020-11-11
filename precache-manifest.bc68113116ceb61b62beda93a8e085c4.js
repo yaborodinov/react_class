81cdf99ea5617ff2b80142fe9c38de93145860fb
Alexandr <yaborodinov@gmail.com>
2020-11-11 22:48:23 +0200

    Updates

---
+++ precache-manifest.bc68113116ceb61b62beda93a8e085c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81539adf2f98900d4cec026f3bd32a99",
+    "url": "/react_class/index.html"
+  },
+  {
+    "revision": "074a0109c5b435280960",
+    "url": "/react_class/static/css/main.9ad16d1b.chunk.css"
+  },
+  {
+    "revision": "0d4cc4aa6c629e08d1e3",
+    "url": "/react_class/static/js/2.cffed1aa.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react_class/static/js/2.cffed1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074a0109c5b435280960",
+    "url": "/react_class/static/js/main.8c484f82.chunk.js"
+  },
+  {
+    "revision": "f508ef035964fe0338d9",
+    "url": "/react_class/static/js/runtime-main.d3c1a2f3.js"
+  }
+]);
